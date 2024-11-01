891fe5cb84224736b7ad30d970c1153de5f65c12
Эльвира <45ark97@gmail.com>
2024-11-01 15:20:48 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
+
 import { Routes,Route } from 'react-router-dom'
-import classes from 'styl es/App.module.scss'
+//import classes from 'styl es/App.module.scss'
 
 function App() {
  
 
   return (
     <Routes>
-<Route path='/login' element={<Login/>}/>
-<Route path='courses' element={<Courses/>}>
-<Route path='index' element={<Index/>}/>
+<Route path='/login' />
+<Route path='courses' >
+<Route path='index' />
 <Route path=':courseId' />
 </Route>
     </Routes>
